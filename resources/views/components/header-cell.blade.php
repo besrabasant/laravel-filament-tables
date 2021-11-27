78ef11f4026879e896000f2f40dab7819cb0b12b
Basant Besra <besrabasant@gmail.com>
2021-11-27 20:42:32 +0530

    Update column sort icon behaviour

--- resources/views/components/header-cell.blade.php
+++ resources/views/components/header-cell.blade.php
@@ -9,11 +9,11 @@
 <th {{ $attributes->merge($extraAttributes)->class(['px-4 py-3', 'filament-tables-header-cell']) }}>
     <button
         @if ($sortable)
-            wire:click="sortTable('{{ $name }}')"
+        wire:click="sortTable('{{ $name }}')"
         @endif
         type="button"
         @class([
-            'flex items-center whitespace-nowrap space-x-1 rtl:space-x-reverse font-medium text-xs text-gray-600',
+            'flex items-center whitespace-nowrap space-x-1 rtl:space-x-reverse font-medium text-xs text-gray-600 uppercase tracking-wider group',
             'cursor-default' => ! $sortable,
         ])
     >
@@ -21,12 +21,15 @@
             {{ $slot }}
         </span>
 
-        @if ($isSortColumn)
-            <span class="relative flex items-center">
+        @if($sortable)
+            <span class="relative flex items-center @unless($sortDirection) opacity-0 group-hover:opacity-100 @endif">
+                @unless($sortDirection)
+                    <x-heroicon-s-selector class="w-3 h-3"/>
+                @endunless
                 @if ($sortDirection === 'asc')
-                    <x-heroicon-s-chevron-up class="w-3 h-3" />
-                @elseif ($sortDirection === 'desc')
                     <x-heroicon-s-chevron-down class="w-3 h-3" />
+                @elseif ($sortDirection === 'desc')
+                    <x-heroicon-s-chevron-up class="w-3 h-3" />
                 @endif
             </span>
         @endif
